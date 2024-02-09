1ffed8e1034da8a293b857f52e8302bc09ef049e
LukasW270 <lwitek270@gmail.com>
2024-02-09 14:43:36 -0500

    updated

---
+++ Example3.java
@@ -0,0 +1,5 @@
+public class Example3 {
+    public static void main(String[]args){
+        System.out.println("Anderson\t3452\nBoyton\t\t5344\nCarrington\t3677\nHouston\t\t5988\nMcDougal\t5233\nRobertson\t3623\nStryker\t\t5122");
+    }
+}
